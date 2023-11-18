5ec68b13fcf1ff680cc32121996aac9681d2fdf1
prabhavathy <prabhavathyjothi15@gmail.com>
2023-11-18 15:31:39 +0530

    New changes in srp(solid principles)

---
+++ solid principle/solid principle/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_principle
+{
+    //srp method
+     public class Employee
+    {
+
+        public string Employename { get; set; }
+
+        public int EmployeMobile { get; set; }
+
+        public void AddEmployee()
+        {
+            Console.WriteLine("Employee Added");
+        }
+
+    }
+}
--- solid principle/solid principle/Program.cs
+++ solid principle/solid principle/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace solid_principle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Employename = "Karthik kishore";
+            employee.EmployeMobile = 987765123;
+            employee.AddEmployee();
+            Console.WriteLine($"Employee Name: {employee.Employename}");
+            Console.WriteLine($"Employee Mobile: {employee.EmployeMobile}");
+            Console.ReadLine();
+        }
+    }
+}
